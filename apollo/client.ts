90fb975d4796a656813f7f78fddf50df385075ac
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2026-02-18 12:41:18 +0900

    feat:deploy luminor client project on docker containers

--- apollo/client.ts
+++ apollo/client.ts
@@ -7,7 +7,6 @@ import { onError } from '@apollo/client/link/error';
 import { getJwtToken } from '../libs/auth';
 import { TokenRefreshLink } from 'apollo-link-token-refresh';
 import { sweetErrorAlert } from '../libs/sweetAlert';
-import error from 'next/error';
 import { socketVar } from './store';
 let apolloClient: ApolloClient<NormalizedCacheObject>;
 
